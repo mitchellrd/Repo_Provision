38ae0e04d9cbb67a6641964e234c7db1fe9d7ed8
mitchellrd <roger.mitchell@elad-si.com>
2014-03-31 15:27:03 -0400

    update and testing

---
+++ src/postgres_otrs.py
@@ -0,0 +1,31 @@
+diff --git a/src/repo_conf.py b/src/repo_conf.py
+new file mode 100644
+index 0000000..fbd2e67
+--- /dev/null
++++ b/src/repo_conf.py
+@@ -0,0 +1,24 @@
++'''
++Created on Mar 30, 2014
++
++@author: roger
++'''
++Linux_OS = {
++        'Type': ['RPM', 'DEBIAN'],
++        'Name': ['Common OTRS Services', 'Local Repository'],
++        'BaseURL': ["ftp://xxx.xx.2.224/pub", "http://LocalRepo.xxx"],
++        'gpgcheck': ["0", "1"],
++        'Enabled': ["0", "1"],
++        't': 'package',
++        }
++
++# Example:
++#  [CS_OTRS_YumServer]
++#  name="Common OTRS Services Yum Repository"
++#  baseurl=ftp://xxx.xxx.xxx.xxx/pub
++#  gpgcheck=0
++#  enabled=1
++#
++# Dependencies:
++#   {}If using FTP each client has to have FTP loaded
++#
+\ No newline at end of file
